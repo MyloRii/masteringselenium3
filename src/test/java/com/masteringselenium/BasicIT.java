5e664c645ee63526798a8c1c2fa8e872e16271ad
shumylo <shumylo.andrii@pdffiller.team>
2019-07-09 22:06:04 +0300

    Added screenshot listener

--- src/test/java/com/masteringselenium/BasicIT.java
+++ src/test/java/com/masteringselenium/BasicIT.java
@@ -1,12 +1,15 @@
 package com.masteringselenium;
 
+import com.masteringselenium.listeners.ScreenshotListener;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(ScreenshotListener.class)
 public class BasicIT extends DriverBase {
 
     private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
